a0a53636808217b4130632b8e70eb9d1ffbcc61e
AlexEe <alexandra.esche@gmx.de>
2019-05-02 17:18:28 +0100

    Add toggle text to about page

--- js/functions.js
+++ js/functions.js
@@ -2,3 +2,12 @@ function thankYou() {
     var popup = document.getElementById("thankYou");
     popup.classList.toggle("show");
 }
+
+function hideText() {
+  var x = document.getElementById("aboutText");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
